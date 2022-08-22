8e6765e27ae53c0b1505dd539acf69decef3bd40
DazFather <the6daz6@gmail.com>
2022-08-22 22:59:42 +0200

    Improve UI for /publish command

--- calendar.go
+++ calendar.go
@@ -1,6 +1,9 @@
 package main
 
-import "time"
+import (
+	"strings"
+	"time"
+)
 
 type CalendarError string
 
@@ -9,6 +12,8 @@ const (
 	INVALID_EVENT    CalendarError = "This date is not avaiable anymore"
 	ALREADY_JOINED   CalendarError = "Event already joined"
 )
+
+const DATETIME_FROMAT = "02/01/2006T15:04"
 
 func (e CalendarError) Error() string {
 	return string(e)
@@ -21,16 +26,76 @@ type Calendar struct {
 	dates       map[string]*Event
 }
 
-func (c *Calendar) addDate(date time.Time) {
-	c.dates[formatDate(date)] = new(Event)
+type Date time.Time
+
+func ParseDate(source string) *Date {
+	switch strings.ToLower(source) {
+	case "current", "today":
+		d := Now()
+		return &d
+	case "tomorrow":
+		d := Now().Skip(0, 0, 1)
+		return &d
+	}
+
+	var date, err = time.Parse(DATETIME_FROMAT, source)
+	if err == nil {
+		d := Date(date)
+		return &d
+	}
+	return nil
 }
 
-func (c *Calendar) joinDate(date time.Time, userID int64) error {
+func Now() Date {
+	return Date(time.Now())
+}
+
+func (d Date) Day() (day int, ofWeek int) {
+	t := d.Time()
+	return t.Day(), int(t.Weekday()) - 1
+}
+
+func (d Date) Week() int {
+	return int(d.Time().Weekday())
+}
+
+func (d Date) Time() time.Time {
+	return time.Time(d)
+}
+
+func (d Date) Format() string {
+	return d.Time().Format(DATETIME_FROMAT)
+}
+
+func (d Date) Month() (month time.Month, maxDays int) {
+	var t = d.Time()
+	return t.Month(), t.AddDate(0, 1, -t.Day()).Day()
+}
+
+func (d Date) MonthStart() Date {
+	var t = d.Time()
+	return Date(t.AddDate(0, 0, -t.Day()+1))
+}
+
+func (d Date) MonthEnd() Date {
+	var t = d.Time()
+	return Date(t.AddDate(0, 1, -t.Day()+1))
+}
+
+func (d Date) Skip(years int, months int, days int) Date {
+	return Date(d.Time().AddDate(years, months, days))
+}
+
+func (c *Calendar) addDate(date Date) {
+	c.dates[date.Format()] = new(Event)
+}
+
+func (c *Calendar) joinDate(date Date, userID int64) error {
 	if c == nil {
 		return INVALID_CALENDAR
 	}
 
-	var event = c.dates[formatDate(date)]
+	var event = c.dates[date.Format()]
 	if event == nil {
 		return INVALID_EVENT
 	}
--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"time"
-	//"fmt"
 	"strconv"
 	"strings"
 
@@ -13,18 +11,8 @@ import (
 	"github.com/NicoNex/echotron/v3"
 )
 
-const DATETIME_FROMAT = "02/01/2006T15:04"
-
-func extractDate(source string) *time.Time {
-	var date, err = time.Parse(DATETIME_FROMAT, source)
-	if err == nil {
-		return &date
-	}
-	return nil
-}
-
-func formatDate(date time.Time) string {
-	return date.Format(DATETIME_FROMAT)
+func beautifyDate(date string) string {
+	return strings.Replace(date, "T", " ", 1)
 }
 
 var organizers = map[int64]*Calendar{}
@@ -64,12 +52,12 @@ var joinHandler = robot.Command{
 			return message.Text{"Invalid joining: " + update.CallbackQuery.Data, nil}
 		} else if userID := retreiveOwner(payload[0]); userID == nil {
 			return message.Text{"Invalid invitation: " + payload[0], nil}
-		} else if d := extractDate(payload[1]); d == nil {
+		} else if d := ParseDate(payload[1]); d == nil {
 			return message.Text{"Invalid date: " + payload[1], nil}
 		} else if err := organizers[*userID].joinDate(*d, bot.ChatID); err != nil {
 			return message.Text{"🚫 " + err.Error(), nil}
 		} else {
-			date, ownerID, calendar = strings.Replace(payload[1], "T", " ", 1), *userID, organizers[*userID]
+			date, ownerID, calendar = beautifyDate(payload[1]), *userID, organizers[*userID]
 		}
 
 		collapse(update.CallbackQuery, "✅ You joined this event")
@@ -84,36 +72,56 @@ var publishHandler = robot.Command{
 	Trigger:     "/publish",
 	ReplyAt:     message.MESSAGE + message.CALLBACK_QUERY,
 	CallFunc: func(bot *robot.Bot, update *message.Update) message.Any {
-		var (
-			date    time.Time
-			dateStr string
-		)
-		if _, payload := extractCommand(update); len(payload) == 0 {
-			return buildCalendarMessage("Select a day")
-		} else if d, err := time.Parse(DATETIME_FROMAT, payload[0]); err != nil {
-			return message.Text{"Invaid date: " + err.Error(), nil}
-		} else {
-			date, dateStr = d, payload[0]
+		var msg message.Any
+
+		switch _, payload := extractCommand(update); len(payload) {
+		case 0:
+			if update.CallbackQuery != nil {
+				update.FromMessage().Delete()
+				return message.Text{"No given payload", nil}
+			}
+			msg = buildCalendarMessage(Now(), "🗓 Select a day from the calendar: ")
+		case 1:
+			var selected Date
+			if d := ParseDate(payload[0]); d == nil {
+				update.FromMessage().Delete()
+				return message.Text{"Invaid date: " + payload[0], nil}
+			} else {
+				selected = *d
+			}
+
+			msg = buildCalendarMessage(selected, "🗓 Select a day from the calendar: ")
+		case 2:
+			var date Date
+			if d := ParseDate(payload[0]); d == nil {
+				update.FromMessage().Delete()
+				return message.Text{"Invaid date: " + payload[0], nil}
+			} else {
+				date = *d
+			}
+
+			if payload[1] == "refresh" {
+				update.FromMessage().Delete()
+				return buildCalendarMessage(date, "🗓 Select a day from the calendar: ")
+			}
+
+			var (
+				hasCalendar bool = organizers[bot.ChatID] != nil
+				calendar         = AddToCalendar(*update.CallbackQuery.From, date)
+			)
+			notify(update.CallbackQuery, "📅 Date: "+beautifyDate(date.Format())+" added to your calendar ✔️")
+			if !hasCalendar {
+				msg = message.Text{
+					strings.Join([]string{
+						"✔️ <i>You calendar has been created</i>",
+						"Use again the previous message to add a new avaiable date",
+						"Share this to make people join: <code>/start " + calendar.invitation + "</code>",
+					}, "\n"),
+					&echotron.MessageOptions{ParseMode: "HTML"},
+				}
+			}
 		}
-
-		if calendar := organizers[bot.ChatID]; calendar != nil {
-			calendar.addDate(date)
-			collapse(update.CallbackQuery, "📅 Date: "+dateStr+" added to your calendar ✔️")
-			return nil
-		}
-
-		calendar := AddCalendar(*update.CallbackQuery.From, date)
-
-		tgui.ShowMessage(
-			*update,
-			strings.Join([]string{
-				"✔️ <i>You calendar has been created</i>",
-				"Use again command /publish to add a new avaiable date",
-				"Share this to make people join: <code>/start " + calendar.invitation + "</code>",
-			}, "\n"),
-			tgui.ParseModeOpt(nil, "HTML"),
-		)
-		return nil
+		return msg
 	},
 }
 
@@ -130,26 +138,34 @@ var alertHandler = robot.Command{
 	Trigger: "/alert",
 	ReplyAt: message.CALLBACK_QUERY,
 	CallFunc: func(bot *robot.Bot, update *message.Update) message.Any {
-		update.CallbackQuery.Answer(&echotron.CallbackQueryOptions{
-			Text:      strings.TrimPrefix(update.CallbackQuery.Data, "/alert"),
-			CacheTime: 3600,
-		})
+		notify(update.CallbackQuery, strings.TrimPrefix(update.CallbackQuery.Data, "/alert"))
 		return nil
 	},
 }
 
-func AddCalendar(user echotron.User, dates ...time.Time) *Calendar {
-	var calendar = &Calendar{
-		name:        user.FirstName + " calendar",
-		description: user.FirstName + " personal event",
-		invitation:  strconv.Itoa(int(user.ID)),
-		dates:       make(map[string]*Event),
+func notify(callback *message.CallbackQuery, text string) {
+	callback.Answer(&echotron.CallbackQueryOptions{
+		Text:      text,
+		CacheTime: 3600,
+	})
+}
+
+func AddToCalendar(user echotron.User, dates ...Date) *Calendar {
+	var calendar *Calendar = organizers[user.ID]
+	if calendar == nil {
+		calendar = &Calendar{
+			name:        user.FirstName + " calendar",
+			description: user.FirstName + " personal event",
+			invitation:  strconv.Itoa(int(user.ID)),
+			dates:       make(map[string]*Event),
+		}
+		organizers[user.ID] = calendar
 	}
+
 	for _, date := range dates {
 		calendar.addDate(date)
 	}
 
-	organizers[user.ID] = calendar
 	return calendar
 }
 
@@ -166,31 +182,53 @@ func collapse(callback *message.CallbackQuery, message string) {
 	callback.Delete()
 }
 
-func buildCalendarMessage(text string) message.Text {
+func buildCalendarMessage(date Date, text string) message.Text {
 	var (
-		now       = time.Now()
-		today     = now.Day()
-		msg       = message.Text{text + "\n" + now.Month().String(), nil}
-		monthDays = now.AddDate(0, 1, -today).Day()
+		month, monthDays = date.Month()
+		weekday          = date.MonthStart().Week()
+		buttons          = make([]tgui.InlineButton, monthDays+weekday)
+		row              []tgui.InlineButton
+		now              = Now().Time()
+		msg              = message.Text{text + month.String(), nil}
 	)
 
-	buttons := make([]tgui.InlineButton, monthDays)
-	for i := 0; i < today; i++ {
+	for i := 0; i < weekday; i++ {
 		buttons[i] = tgui.InlineCaller("🚫", "/alert", "❌ Cannot create an event in this day")
 	}
-	for i := today + 1; i <= monthDays; i++ {
-		buttons[i-1] = tgui.InlineCaller(
-			strconv.Itoa(i),
-			"/publish",
-			formatDate(now.AddDate(0, 0, i-today)),
+
+	row = buttons[weekday:]
+	date = date.MonthStart()
+	for i := range row {
+		var (
+			label string = strconv.Itoa(i + 1)
+			day   Date   = date.Skip(0, 0, i)
 		)
+
+		if day.Time().Before(now) {
+			row[i] = tgui.InlineCaller("🚫"+label, "/alert", "❌ Cannot create an event in this day")
+		} else {
+			row[i] = tgui.InlineCaller(label, "/publish", day.Format(), "add")
+		}
 	}
 
-	msg.ClipInlineKeyboard(append(
-		tgui.Arrange(7, buttons...),
-		tgui.Wrap(tgui.InlineCaller("❌ Cancel", "/close", "✔️ Operation cancelled")),
+	keyboard := tgui.Arrange(7, buttons...)
+	row = keyboard[len(keyboard)-1]
+	for i := len(row); i < 7; i++ {
+		row = append(row, tgui.InlineCaller("🚫", "/alert", "❌ Cannot create an event in this day"))
+	}
+	keyboard[len(keyboard)-1] = row
+
+	return *msg.ClipInlineKeyboard(append(
+		[][]tgui.InlineButton{{
+			tgui.InlineCaller("⏮", "/publish", date.Skip(0, -1, 0).Format(), "refresh"),
+			tgui.InlineCaller(month.String(), "/alert", "This feature is not yet avaiable"),
+			tgui.InlineCaller("⏭", "/publish", date.Skip(0, 1, 0).Format(), "refresh"),
+		}},
+		append(keyboard, []tgui.InlineButton{
+			tgui.InlineCaller("❌ Cancel", "/close", "✔️ Operation cancelled"),
+			tgui.InlineCaller("🔄Refresh", "/publish", date.Format(), "refresh"),
+		})...,
 	))
-	return msg
 }
 
 func extractCommand(update *message.Update) (command string, payload []string) {
@@ -231,7 +269,7 @@ func buildDateListMessage(c Calendar, userID int64) message.Text {
 	)
 
 	for date, event := range c.dates {
-		var caption string = strings.Replace(date, "T", " ", 1)
+		var caption string = beautifyDate(date)
 		if n := event.countAttendee(); n > 0 {
 			if event.hasJoined(userID) {
 				caption = "✅ " + caption + " - 👥" + strconv.Itoa(n-1) + " + 1 (You)"
